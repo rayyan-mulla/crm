0cbcb95dd6cb2114f1e38be8c644ba0ac030bc88
Rayyan Mulla <rayyan.mulla@t-systems.com>
2025-09-23 16:29:16 +0530

    Modify the server startup code

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.use((err, req, res, next) => {
 });
 
 // --- Start server ---
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
