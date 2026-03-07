ca3ea21ab19cc169bb81e371d54004350d989089
Rayyan Mulla <mullarayyan07@gmail.com>
2026-03-07 17:30:06 +0530

    fix(auth): add isAdmin middleware to vendor routes for proper authentication

--- routes/vendors.js
+++ routes/vendors.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const vendorController = require('../controllers/vendorController');
+const { isAdmin } = require('../middlewares/auth');
 
+router.use(isAdmin);
 
 router.get('/', vendorController.index);
-
 router.get('/new', vendorController.newForm);
-
 router.post('/', vendorController.create);
 
 router.get('/:id/edit', vendorController.editForm);
-
 router.post('/:id', vendorController.update);
-
 router.post('/:id/delete', vendorController.destroy);
 
 
